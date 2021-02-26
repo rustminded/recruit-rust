dd774966e65ca4b675801873b745c4d6f21a4315
yozhgoor <yozhgoor@outlook.com>
2021-02-26 19:30:38 +0100

    All the jobs informations are printed now

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,5 +1,6 @@
 use candidate::Job;
 use yew::prelude::*;
+use yewprint::{Tag, Text};
 
 pub struct Jobs {
     props: Props,
@@ -27,9 +28,31 @@ impl Component for Jobs {
     }
 
     fn view(&self) -> Html {
+        let jobs_tags = self
+            .props
+            .candidate_jobs
+            .tech
+            .iter()
+            .map(|x| {
+                html! {
+                    <Tag>
+                        {x}
+                    </Tag>
+                }
+            })
+            .collect::<Html>();
+
         html! {
             <div>
-                <p>{"Hello, world!"}</p>
+                <Text>{self.props.candidate_jobs.company}</Text>
+                <a href={self.props.candidate_jobs.website}>
+                    {"Website"}
+                </a>
+                <Text>{self.props.candidate_jobs.description}</Text>
+                <div>
+                    {jobs_tags}
+                </div>
+                <Text>{self.props.candidate_jobs.period}</Text>
             </div>
         }
     }
