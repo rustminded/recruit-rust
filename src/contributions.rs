bbd1ec6e8aa1c1e4e275137cb8f2115abd639366
yozhgoor <yozhgoor@outlook.com>
2021-03-01 20:10:54 +0100

    Correct namming for the props

--- src/contributions.rs
+++ src/contributions.rs
@@ -3,17 +3,17 @@ use yew::prelude::*;
 use yewprint::{Tag, Text};
 
 pub struct Contributions {
-    props: Props,
+    props: ContributionProps,
 }
 
 #[derive(Debug, Properties, PartialEq, Clone)]
-pub struct Props {
+pub struct ContributionProps {
     pub contributions: &'static Contribution,
 }
 
 impl Component for Contributions {
     type Message = ();
-    type Properties = Props;
+    type Properties = ContributionProps;
 
     fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
         Contributions { props }
