680142ad7c7ae2302ba0e35ad7d156b8f12c4057
yozhgoor <yozhgoor@outlook.com>
2021-03-02 21:05:46 +0100

    Override the serve hook

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use std::process::Command;
 use structopt::StructOpt;
 use wasm_run::prelude::*;
 
-#[wasm_run::main(pre_build = pre_build)]
+#[wasm_run::main(
+    pre_build = pre_build,
+    serve = serve,
+)]
 #[derive(StructOpt, Debug)]
 enum Cli {}
 
@@ -19,5 +22,30 @@ fn pre_build(_args: &DefaultBuildArgs, profile: BuildProfile, command: &mut Comm
         }
     }
 
+    Ok(())
+}
+
+fn serve(args: &DefaultServeArgs, server: &mut Server<()>) -> Result<()> {
+    use tide::{Body, Request, Response};
+
+    let build_path = args.build_args().build_path().to_owned();
+    let index_path = build_path.join("index.html").to_owned();
+
+    server.at("/").serve_dir(args.build_args().build_path())?;
+    server.at("/").get(move |_| {
+        let index_path = index_path.clone();
+        async move { Ok(Response::from(Body::from_file(index_path).await?)) }
+    });
+    server.at("/*path").get(move |req: Request<()>| {
+        let build_path = build_path.clone();
+        async move {
+            match Body::from_file(build_path.join(req.param("path").unwrap())).await {
+                Ok(body) => Ok(Response::from(body)),
+                Err(_) => Ok(Response::from(
+                    Body::from_file(build_path.join("index.html")).await?,
+                )),
+            }
+        }
+    });
     Ok(())
 }
