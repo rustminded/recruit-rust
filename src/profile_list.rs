78b44a114c3bc8eef5d4688cc90a4c8108b994bf
yozhgoor <yozhgoor@outlook.com>
2021-03-02 14:43:54 +0100

    Add the content of profile.rs

---
+++ src/profile_list.rs
@@ -0,0 +1,33 @@
+use yew::prelude::*;
+
+pub struct ProfileList {
+    props: ProfileListProps,
+}
+
+#[derive(Debug, Properties, PartialEq, Clone)]
+pub struct ProfileProps {
+    pub candidate: &'static Candidate,
+}
+
+impl Component for ProfileList {
+    type Message = ();
+    type Properties = ProfileListProps;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        ProfileList { props }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p>{"Hello, world!"}</p>
+        }
+    }
+}
