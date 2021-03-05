3261a603c8b3f7f3819251f7b8f0d56f6b6d4a57
yozhgoor <yozhgoor@outlook.com>
2021-03-05 13:21:31 +0100

    WIP

--- src/app.rs
+++ src/app.rs
@@ -24,10 +24,81 @@ pub struct Tech {
     public: bool,
 }
 
+impl Tech {
+    fn from_candidate(candidate: &'static Candidate) -> Tech {
+        pub struct Tech {
+            tech: &'static str,
+            professional: bool,
+            public: bool,
+        }
+
+        impl Tech {
+            fn from_candidate(candidate: &'static Candidate) -> HashSet<Tech> {
+                // Create the HashSet who collect the techs
+                let mut techs: HashSet<Tech> = HashSet::new();
+
+                // Convert the asked_techs (&str) in type Tech
+
+                // put the asked_techs in the techs HashSet
+                techs.extend(candidate.asked_techs);
+
+                /*
+                // Collect the jobs_techs
+                let jobs_techs = candidate
+                    .jobs
+                    .iter()
+                    .map(|x| x.techs)
+                    .collect::<HashSet<&[&str]>>();
+
+                // Convert the jobs_techs (&[&str]) in type Tech
+
+                // Put jobs_techs in the techs HashSet
+                for s in jobs_techs.iter() {
+                    techs.extend(s.iter());
+                }
+
+                // Collect the contribs_techs
+                let contribs_techs = candidate
+                    .contributions
+                    .iter()
+                    .map(|x| x.techs)
+                    .collect::<HashSet<&[&str]>>();
+
+                // Convert the contribs_techs (&[&str]) in type Tech
+
+                // Put the contribs_techs in the techs HashSet
+                for s in contribs_techs {
+                    techs.extend(s.iter());
+                }
+
+                // Collect the personal_techs
+                let personal_techs = candidate
+                    .personal_projects
+                    .iter()
+                    .map(|x| x.techs)
+                    .collect::<HashSet<&[&str]>>();
+
+                // Convert the personal_techs (&[&str]) in type Tech
+
+                // Put the personal_techs in the techs HashSet
+                for s in personal_techs {
+                    techs.extend(s.iter());
+                }
+                */
+
+                HashSet<Tech>
+            }
+        }
+    }
+}
+
 impl CandidateInfo {
-    fn from_candidate(candidate_info: &'static Candidate, url: &'static str) -> CandidateInfo {
+    fn from_candidate(
+        candidate_info: &'static Candidate,
+        candidate_url: &'static str,
+    ) -> CandidateInfo {
         let candidate = candidate_info;
-        let url = url;
+        let url = candidate_url;
         let mut techs: HashSet<&str> = HashSet::new();
         techs.extend(candidate.asked_techs);
         let jobs_techs = candidate
