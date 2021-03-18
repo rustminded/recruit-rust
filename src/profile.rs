b056be6a730e7a67b9e3394ecc3e67fac94967be
yozhgoor <yozhgoor@outlook.com>
2021-03-18 12:26:55 +0100

    Make same with jobs tags and contribs

--- src/profile.rs
+++ src/profile.rs
@@ -44,8 +44,13 @@ impl Component for Profile {
         true
     }
 
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        true
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.props != props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
     }
 
     fn view(&self) -> Html {
