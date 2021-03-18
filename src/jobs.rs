9c04da1bf2abd572de660fda2130e6c62b2a9aff
yozhgoor <yozhgoor@outlook.com>
2021-03-18 14:38:16 +0100

    Revert "Revert "Revert "Make same with jobs tags and contribs"""
    This reverts commit eadb49e19a051ac10c0ada1005bb6486555692ff.

--- src/jobs.rs
+++ src/jobs.rs
@@ -4,7 +4,6 @@ use yewprint::{Intent, Tag, Text};
 
 pub struct Jobs {
     props: JobsProps,
-    link: ComponentLink<Self>,
 }
 
 #[derive(Debug, Properties, PartialEq, Clone)]
@@ -13,40 +12,20 @@ pub struct JobsProps {
     pub highlighted_tech: Option<String>,
 }
 
-pub enum Msg {
-    Click(String),
-}
-
 impl Component for Jobs {
-    type Message = Msg;
+    type Message = ();
     type Properties = JobsProps;
 
-    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Jobs { props, link }
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Jobs { props }
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::Click(value) => {
-                if let Some(highlighted_tech) = self.props.highlighted_tech.as_mut() {
-                    highlighted_tech.clear();
-                    highlighted_tech.push_str(&value);
-                } else {
-                    let highlighted_tech = String::from(&value);
-                    self.props.highlighted_tech = Some(highlighted_tech);
-                }
-            }
-        }
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
         true
     }
 
-    fn change(&mut self, props: Self::Properties) -> ShouldRender {
-        if self.props != props {
-            self.props = props;
-            true
-        } else {
-            false
-        }
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
     }
 
     fn view(&self) -> Html {
@@ -59,14 +38,12 @@ impl Component for Jobs {
                 html! {
                     <Tag
                         class=classes!("tag")
-                        interactive=true
                         intent={
                             match self.props.highlighted_tech.as_ref() {
                                 Some(highlighted_tech) if highlighted_tech == x => Intent::Danger,
                                 _ => Intent::Warning
                             }
                         }
-                        onclick=self.link.callback(move |_| Msg::Click(x.to_string()))
                     >
                         {x}
                     </Tag>
