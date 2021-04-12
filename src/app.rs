69fca332ac3252ed021d6e6bb896dd84826d66c1
Yozhgoor <yozhgoor@outlook.com>
2021-04-12 13:59:09 +0200

    Add an not working slider

--- src/app.rs
+++ src/app.rs
@@ -7,19 +7,21 @@ use std::collections::HashMap;
 use std::rc::Rc;
 use yew::prelude::*;
 use yew_router::{router::Router, Switch};
-use yewprint::{Button, IconName, InputGroup, Text, H1, H2};
+use yewprint::{Button, IconName, InputGroup, Slider, Text, H1, H2};
 
 pub struct App {
     candidates: Rc<HashMap<&'static str, CandidateInfo>>,
     link: ComponentLink<Self>,
     entries: Rc<TechSet>,
     value: String,
+    selected_timezone: i32,
 }
 
 pub enum Msg {
     AddEntry,
     Update(String),
     Nope,
+    SelectTimezone(i32),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -75,6 +77,7 @@ impl Component for App {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         let entries = TechSet::new();
         let value = String::new();
+        let selected_timezone = 0;
         let mut candidates = HashMap::new();
         let candidate_1_info = CandidateInfo::from_candidate(yozhgoor::candidate(), "yozhgoor");
         let candidate_2_info = CandidateInfo::from_candidate(yozhgoor::candidate(), "yozhgoor2");
@@ -89,6 +92,7 @@ impl Component for App {
             link,
             entries,
             value,
+            selected_timezone,
         }
     }
 
@@ -104,6 +108,9 @@ impl Component for App {
             }
             Msg::Update(val) => {
                 self.value = val;
+            }
+            Msg::SelectTimezone(tz) => {
+                self.selected_timezone = tz;
             }
             Msg::Nope => {}
         }
@@ -142,9 +149,46 @@ impl Component for App {
                         />
                     </div>
                 </div>
-                <Text class=classes!("app-intro")>
-                    {"The place to be hired as an awesome Rustacean"}
-                </Text>
+                <div class=classes!("app-top")>
+                    <Text class=classes!("app-intro")>
+                        {"The place to be hired as an awesome Rustacean"}
+                    </Text>
+                    <Slider<i32>
+                        selected=self.selected_timezone
+                        values=vec![
+                            (-12, Some(String::from("UTC -12"))),
+                            (-11, Some(String::from("UTC -11"))),
+                            (-10, Some(String::from("UTC -10"))),
+                            (-9, Some(String::from("UTC -9"))),
+                            (-8, Some(String::from("UTC -8"))),
+                            (-7, Some(String::from("UTC -7"))),
+                            (-6, Some(String::from("UTC -6"))),
+                            (-5, Some(String::from("UTC -5"))),
+                            (-4, Some(String::from("UTC -4"))),
+                            (-3, Some(String::from("UTC -3"))),
+                            (-2, Some(String::from("UTC -2"))),
+                            (-1, Some(String::from("UTC -1"))),
+                            (0, Some(String::from("UTC"))),
+                            (1, Some(String::from("UTC + 1"))),
+                            (2, Some(String::from("UTC + 2"))),
+                            (3, Some(String::from("UTC + 3"))),
+                            (4, Some(String::from("UTC + 4"))),
+                            (5, Some(String::from("UTC + 5"))),
+                            (6, Some(String::from("UTC + 6"))),
+                            (7, Some(String::from("UTC + 7"))),
+                            (8, Some(String::from("UTC + 8"))),
+                            (9, Some(String::from("UTC + 9"))),
+                            (10, Some(String::from("UTC + 10"))),
+                            (11, Some(String::from("UTC + 11"))),
+                            (12, Some(String::from("UTC + 12"))),
+                            (13, Some(String::from("UTC + 13"))),
+                            (14, Some(String::from("UTC + 14"))),
+                        ]
+                        onchange=self.link.callback(|x| Msg::SelectTimezone(x))
+
+                    />
+
+                </div>
                 <Text class=classes!("reddit-link")>
                     {"If you want to share an opportunity, check this "}
                     <a
